fff164e11703cc39a510467b44f358c7b25c4210
Andrew Heisenberg <0x5000000@gmail.com>
2017-04-05 12:27:38 +0100

    Update support_sfinv.lua

--- invrec/support_sfinv.lua
+++ invrec/support_sfinv.lua
@@ -1,17 +1,17 @@
 --------------------------------------------------------------
 -- Native support 
 --------------------------------------------------------------
-sfinv.register_page(invrec.m_Name, {
-	title = invrec.m_Title,
+sfinv.register_page("invrecipe:recipes", {
+	title = invrec.title,
 	get = function(self, player, context)
-		return sfinv.make_formspec(player, context, invrec._f, not invrec.m_isFull)
+		return sfinv.make_formspec(player, context, invrec._f, false)
 	end,
 	on_player_receive_fields = function(self, player, context, fields)
 		invrec.events(self, player, context, fields);
 		sfinv.set_player_inventory_formspec(player, context);
 	end,
 	on_enter = function(self, player, context)
-		invrec.events(self, player, context, fields);
+		invrec.events(self, player, context, nil);
 		sfinv.set_player_inventory_formspec(player, context);
 	end
 });
