b8a317292609ff0bc3014ed3ab1767a9a3b2bf3f
Andrew Heisenberg <0x5000000@gmail.com>
2017-04-13 11:31:24 +0100

    Update support_ipp.lua

--- invrec/support_ipp.lua
+++ invrec/support_ipp.lua
@@ -1,20 +1,19 @@
 --------------------------------------------------------------
--- Inventory Plus I/O
+-- Inventory Plus/++ I/O
 --------------------------------------------------------------
 minetest.register_on_joinplayer(function(player)
 	inventory_plus.register_button(player,"invrec",invrec.title)
 end)
 
 minetest.register_on_player_receive_fields(function(player,formname,fields)
-	if(fields.main) then -- back to default page
-	    inventory_plus.get_formspec(player, inventory_plus.default);
-		return;
+	if fields.main then -- back to default page
+		inventory_plus.get_formspec(player, inventory_plus.default)
+		return
 	end
-	if(fields.invrec) then
-		-- handle events
-		invrec.events(nil, player, nil, fields);
-		
-		inventory_plus.set_inventory_formspec(player,  "size[8,7.5] bgcolor[#080808BB;true] field[0,0;0,0;invrec;;] button[0,0.4;2,.5;main;Back]" 
-				.. default.gui_bg .. default.gui_bg_img .. invrec._f) -- DUCT TAPE: Add default style and fake field "invrec"
+	if fields.invrec then
+		if invrec.events(nil, player, nil, fields) then 
+			inventory_plus.set_inventory_formspec(player,  "size[8,7.5] bgcolor[#080808BB;true] button[0,0.4;2,.5;main;Back]" 
+				.. default.gui_bg .. default.gui_bg_img .. invrec.update_gui(player))
+		end
 	end
 end)
